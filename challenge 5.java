d9d76e4eff59a9db313f6c1aa28b53ba4a0b0c0b
Issaa-Yano <kiptoontrevor2@gmail.com>
2025-05-05 14:51:11 +0300

    added challenge 5

---
+++ challenge 5.java
@@ -0,0 +1,10 @@
+public class CountToFive {
+    public static void main(String[] args) {
+        int number = 1;
+
+        while (number <= 5) {
+            System.out.println("Number: " + number);
+            number++;
+        }
+    }
+}
