a11618a441859b37c95f9043d56583b1f88bc31b
Issaa-Yano <kiptoontrevor2@gmail.com>
2025-05-05 14:22:42 +0300

    added challenge 1

---
+++ Greeter.java
@@ -0,0 +1,12 @@
+
+    
+// This is a simple Java program that prints "Hi there" to the console
+//puts message on the screen
+public class Greeter {
+    void greet(){
+        System.out.println("Hi there");
+    }
+    public static void main(String[] args){
+    (new Greeter()).greet();
+    }
+    }
